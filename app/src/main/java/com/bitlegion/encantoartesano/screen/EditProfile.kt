26a6d723f40f0a1f4ab56a3aed224f26b30f67ef
Henmart02 <00012622@uca.edu.sv>
2024-06-25 01:22:38 -0600

    edit perfil

--- app/src/main/java/com/bitlegion/encantoartesano/screen/EditProfile.kt
+++ app/src/main/java/com/bitlegion/encantoartesano/screen/EditProfile.kt
@@ -16,12 +16,10 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import com.bitlegion.encantoartesano.Api.ApiClient
 import com.bitlegion.encantoartesano.Api.User
 import com.bitlegion.encantoartesano.MainViewModel
@@ -33,7 +31,39 @@ fun EditProfileScreen(navController: NavController, viewModel: MainViewModel, us
     var name by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     var age by remember { mutableStateOf("") }
+    var errorMessage by remember { mutableStateOf("") }
     val coroutineScope = rememberCoroutineScope()
+
+    var isNameValid by remember { mutableStateOf(false) }
+    var isPasswordValid by remember { mutableStateOf(false) }
+    var isAgeValid by remember { mutableStateOf(false) }
+
+    fun validateName() {
+        isNameValid = name.isNotBlank()
+    }
+
+    fun checkPassword() {
+        isPasswordValid = password.length >= 8 &&
+                password.any { it.isUpperCase() } &&
+                password.any { it.isLowerCase() } &&
+                password.any { it.isDigit() }
+    }
+
+    fun checkAge() {
+        isAgeValid = age.toIntOrNull()?.let { it >= 18 } ?: false
+    }
+
+    LaunchedEffect(name) {
+        validateName()
+    }
+
+    LaunchedEffect(password) {
+        checkPassword()
+    }
+
+    LaunchedEffect(age) {
+        checkAge()
+    }
 
     Column(
         modifier = Modifier
@@ -74,23 +104,45 @@ fun EditProfileScreen(navController: NavController, viewModel: MainViewModel, us
         Spacer(modifier = Modifier.height(16.dp))
         AccountEditable(label = "Nombre", value = name, onValueChange = { name = it })
         Spacer(modifier = Modifier.height(16.dp))
-        AccountEditable(label = "Contraseña", value = password, onValueChange = { password = it })
+        Text(
+            text = "El nombre de usuario es el nombre único con el que te identificarás en la plataforma.",
+            style = MaterialTheme.typography.body2.copy(color = Color.White),
+            modifier = Modifier.padding(horizontal = 32.dp)
+        )
+        PasswordEditable(label = "Contraseña", value = password, onValueChange = { password = it })
         Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = "Asegúrate de que tu contraseña tenga al menos 8 caracteres, una mayúscula, una minúscula y un número.",
+            style = MaterialTheme.typography.body2.copy(color = Color.White),
+            modifier = Modifier.padding(horizontal = 32.dp)
+        )
         AgeEdit(label = "Edad", value = age, onValueChange = { age = it })
         Spacer(modifier = Modifier.height(32.dp))
+        Text(
+            text = "Debes ser mayor de 18 años para registrarte en nuestra plataforma.",
+            style = MaterialTheme.typography.body2.copy(color = Color.White),
+            modifier = Modifier.padding(horizontal = 32.dp)
+        )
         Button(
             onClick = {
                 coroutineScope.launch {
-                    val updatedUser = User(name, password, age.toInt())
-                    val response = ApiClient.apiService.updateUser(userId, updatedUser)
-                    if (response.isSuccessful) {
-                        navController.popBackStack()
+                    if (isNameValid && isPasswordValid && isAgeValid) {
+                        val updatedUser = User(name, password, age.toInt())
+                        val response = ApiClient.apiService.updateUser(userId, updatedUser)
+                        if (response.isSuccessful) {
+                            navController.popBackStack()
+                        } else {
+                            errorMessage = "Error al actualizar el perfil"
+                        }
+                    } else {
+                        errorMessage = "Por favor, completa todos los campos correctamente."
                     }
                 }
             },
             colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFFE19390)),
             shape = RoundedCornerShape(16.dp),
-            modifier = Modifier.align(Alignment.CenterHorizontally)
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            enabled = isNameValid && isPasswordValid && isAgeValid
         ) {
             Text(
                 text = "Guardar",
@@ -98,9 +150,12 @@ fun EditProfileScreen(navController: NavController, viewModel: MainViewModel, us
                 fontWeight = FontWeight.Bold
             )
         }
+        if (errorMessage.isNotEmpty()) {
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(text = errorMessage, color = Color.Red)
+        }
     }
 }
-
 
 @Composable
 fun AccountEditable(label: String, value: String, onValueChange: (String) -> Unit) {
@@ -130,7 +185,20 @@ fun AccountEditable(label: String, value: String, onValueChange: (String) -> Uni
 }
 
 @Composable
-fun AgeEdit(label: String, value: String, onValueChange: (String) -> Unit) {
+fun PasswordEditable(label: String, value: String, onValueChange: (String) -> Unit) {
+    var isPasswordValid by remember { mutableStateOf(false) }
+
+    fun checkPassword() {
+        isPasswordValid = value.length >= 8 &&
+                value.any { it.isUpperCase() } &&
+                value.any { it.isLowerCase() } &&
+                value.any { it.isDigit() }
+    }
+
+    LaunchedEffect(value) {
+        checkPassword()
+    }
+
     Column(modifier = Modifier.fillMaxWidth()) {
         Text(
             text = label,
@@ -141,25 +209,71 @@ fun AgeEdit(label: String, value: String, onValueChange: (String) -> Unit) {
         Spacer(modifier = Modifier.height(8.dp))
         OutlinedTextField(
             value = value,
-            onValueChange = onValueChange,
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            onValueChange = { onValueChange(it); checkPassword() },
+            visualTransformation = PasswordVisualTransformation(),
+            singleLine = true,
+            placeholder = { Text("Introduce tu contraseña") },
             modifier = Modifier
                 .fillMaxWidth()
                 .background(Color(0xFFD0CFBC), RoundedCornerShape(8.dp)),
-            singleLine = true,
-
             colors = TextFieldDefaults.outlinedTextFieldColors(
                 textColor = Color.Black,
                 focusedBorderColor = Color.Black,
                 unfocusedBorderColor = Color.Black,
                 backgroundColor = Color(0xFFD0CFBC)
-            )
+            ),
+            trailingIcon = {
+                Icon(
+                    painter = painterResource(id = R.drawable.baseline_check_24),
+                    contentDescription = null,
+                    tint = if (isPasswordValid) Color.Green else Color.Red
+                )
+            }
         )
     }
 }
-/*
-@Preview(showBackground = true)
+
 @Composable
-fun PreviewEditProfile() {
-    EditProfileScreen(navController = rememberNavController())
-}*/
+fun AgeEdit(label: String, value: String, onValueChange: (String) -> Unit) {
+    var isAgeValid by remember { mutableStateOf(false) }
+
+    fun checkAge() {
+        isAgeValid = value.toIntOrNull()?.let { it >= 18 } ?: false
+    }
+
+    LaunchedEffect(value) {
+        checkAge()
+    }
+
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Text(
+            text = label,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        OutlinedTextField(
+            value = value,
+            onValueChange = { onValueChange(it); checkAge() },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            singleLine = true,
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color(0xFFD0CFBC), RoundedCornerShape(8.dp)),
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                textColor = Color.Black,
+                focusedBorderColor = Color.Black,
+                unfocusedBorderColor = Color.Black,
+                backgroundColor = Color(0xFFD0CFBC)
+            ),
+            trailingIcon = {
+                Icon(
+                    painter = painterResource(id = R.drawable.baseline_check_24),
+                    contentDescription = null,
+                    tint = if (isAgeValid) Color.Green else Color.Red
+                )
+            }
+        )
+    }
+}
